620a3530a2fb3e4e9de15281b268e0b556952988
Ankur Bombarde <145347832+AnkurBombarde@users.noreply.github.com>
2024-05-01 15:42:54 +0530

    Update Selection.java

--- Selection.java
+++ Selection.java
@@ -1,4 +1,4 @@
-/*Implement Greedy search algorithm for any of the following application: I. Selection Sort II. Minimum Spanning Tree III. Single-Source Shortest Path Problem IV. Job Scheduling Problem V. Prim's Minimal Spanning Tree Algorithm VI. Kruskal's Minimal Spanning Tree Algorithm VII. Dijkstra's Minimal Spanning Tree Algorithm */
+/*3. Implement Greedy search algorithm for any of the following application: I. Selection Sort II. Minimum Spanning Tree III. Single-Source Shortest Path Problem IV. Job Scheduling Problem V. Prim's Minimal Spanning Tree Algorithm VI. Kruskal's Minimal Spanning Tree Algorithm VII. Dijkstra's Minimal Spanning Tree Algorithm */
 
 import java.util.Scanner;
 
