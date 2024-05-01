cac1a7e29438423e845d7105f070bf4d6f29bbc4
Ankur Bombarde <145347832+AnkurBombarde@users.noreply.github.com>
2024-05-01 15:24:05 +0530

    DFSBFS.java

--- DFSBFS.java
+++ DFSBFS.java
@@ -1,3 +1,5 @@
+/*1. Implement depth first search algorithm and Breadth First Search algorithm, Use an undirected graph and develop a recursive algorithm for searching all the vertices of a graph or tree data structure.*/
+
 import java.util.*;
 
 class Graph {
