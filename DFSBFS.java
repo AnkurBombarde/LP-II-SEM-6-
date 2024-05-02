c76ebe585b3902e887410ee5844fd0adc776827a
Ankur Bombarde <145347832+AnkurBombarde@users.noreply.github.com>
2024-05-02 08:50:28 +0530

    DFSBFS.java

--- DFSBFS.java
+++ DFSBFS.java
@@ -5,40 +5,43 @@ import java.util.*;
 class Graph {
     private int V;
     private LinkedList<Integer> adj[];
- 
+
     Graph(int v) {
         V = v;
         adj = new LinkedList[v];
         for (int i = 0; i < v; ++i)
             adj[i] = new LinkedList<>();
     }
- 
+
     void addEdge(int v, int w) {
         adj[v].add(w);
     }
- 
-    void DFSUtil(int v, boolean visited[]) {
-        visited[v] = true;
-        System.out.print(v + " ");
-        Iterator<Integer> i = adj[v].listIterator();
-        while (i.hasNext()) {
-            int n = i.next();
-            if (!visited[n])
-                DFSUtil(n, visited);
-        }
-    }
- 
+
     void DFS(int v) {
         boolean visited[] = new boolean[V];
-        DFSUtil(v, visited);
+        LinkedList<Integer> stack = new LinkedList<>();
+        visited[v] = true;
+        stack.push(v);
+        while (!stack.isEmpty()) {
+            v = stack.pop();
+            System.out.print(v + " ");
+            Iterator<Integer> i = adj[v].listIterator();
+            while (i.hasNext()) {
+                int n = i.next();
+                if (!visited[n]) {
+                    visited[n] = true;
+                    stack.push(n);
+                }
+            }
+        }
     }
- 
+
     void BFS(int s) {
         boolean visited[] = new boolean[V];
         LinkedList<Integer> queue = new LinkedList<>();
         visited[s] = true;
         queue.add(s);
-        while (queue.size() != 0) {
+        while (!queue.isEmpty()) {
             s = queue.poll();
             System.out.print(s + " ");
             Iterator<Integer> i = adj[s].listIterator();
@@ -51,23 +54,24 @@ class Graph {
             }
         }
     }
- }
- 
- public class DFSBFS {
+}
+
+public class DFSBFS {
     public static void main(String args[]) {
         Graph g = new Graph(4);
- 
+
         g.addEdge(0, 1);
         g.addEdge(0, 2);
         g.addEdge(1, 2);
         g.addEdge(2, 0);
         g.addEdge(2, 3);
         g.addEdge(3, 3);
- 
+
         System.out.println("Depth First Traversal (starting from vertex 2): ");
         g.DFS(2);
- 
+
         System.out.println("\nBreadth First Traversal (starting from vertex 2): ");
         g.BFS(2);
     }
- }
+}
+
